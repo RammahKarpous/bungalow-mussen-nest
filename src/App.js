3dea850488129ef494e3a7c1bfce004b47b0fac0
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-06-04 12:38:09 +0100

    Removed the open and close navigation components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navigation from './components/navigation/Navigation';
-import OpenNav from './components/navigation/OpenNav';
 import Home from './pages/Home';
 
 function App() {
+
   return (
     <BrowserRouter>
       <Navigation />
-      <OpenNav />
 
       <Switch>
         <Route exact path="/" component={Home} />
--- src/components/navigation/OpenNav.jsx
+++
@@ -1,9 +0,0 @@
-const OpenNav = () => {
-    return (
-        <div className="h-20 w-20 bg-green-100 flex items-center justify-center text-gray-300">
-            <p className="font-semibold uppercase tracking-widest">menu</p>
-        </div>
-    )
-}
-
-export default OpenNav
