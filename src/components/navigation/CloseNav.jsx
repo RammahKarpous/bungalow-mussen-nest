b9061d17ce14c85bcf2e277e3d5aa185f6f16a4e
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-06-04 12:24:52 +0100

    Update CloseNav.jsx

--- src/components/navigation/CloseNav.jsx
+++ src/components/navigation/CloseNav.jsx
@@ -1,6 +1,6 @@
 const CloseNav = () => {
     return (
-        <div className="relative">
+        <div className="absolute top-3 left-6 h-10 w-10 sm:invisible">
             <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 rotate-45 bg-gray-300 w-1 h-9"></div>
             <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 -rotate-45 bg-gray-300 w-1 h-9"></div>
         </div>
