89a7a450ad8a3481c14a91a7fedabf1acad22c0f
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-06-04 12:23:16 +0100

    Created the links

---
+++ src/components/links.js
@@ -0,0 +1,16 @@
+import home from '../assets/icons/home.svg'
+import afbeelding from '../assets/icons/afbeelding.svg'
+import map from '../assets/icons/map.svg'
+import kalender from '../assets/icons/kalender.svg'
+import fiets from '../assets/icons/fiets.svg'
+import envelop from '../assets/icons/envelop.svg'
+
+
+export const links = [
+    { to: '/', name: 'Home', icon: home },
+    { to: '/tuin', name: 'Gallerij', icon: afbeelding },
+    { to: '/over-de-bungalow', name: 'Over de bungalow', icon: map },
+    { to: '/reserveren', name: 'Reserveren', icon: kalender },
+    { to: '/activiteiten', name: 'Activiteiten', icon: fiets },
+    { to: '/contact-ons', name: 'Contact ons', icon: envelop },
+]
