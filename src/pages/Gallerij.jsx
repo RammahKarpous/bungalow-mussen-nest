ec2fa878c7d63e34a3b0c295e19881d4c7e05adb
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-06-06 21:18:11 +0100

    Update Gallerij.jsx

--- src/pages/Gallerij.jsx
+++ src/pages/Gallerij.jsx
@@ -4,6 +4,7 @@ import { gallerijLinks } from '../components/links'
 const Gallerij = ({ page, active }) => {
     return (
         <div className="col-start-1 lg:col-start-2 col-end-7 lg:col-end-9 relative mt-20">
+            <h1>{active}</h1>
             <TopNavigation type={ gallerijLinks } active={active} />
             {page}
         </div>
