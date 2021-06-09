43fc4a17638526d78bec3207ec6991365edd0a8b
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-06-09 19:44:50 +0100

    Updated the button component to allow pdf downloads

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,26 @@
 import { Link } from "react-router-dom"
 
-const Button = ({ type, to, text }) => {
-    return (
-        <Link to={to} className={`flex justify-center items-center py-3 px-5 uppercase font-body font-bold ${type}`}>
-            {text}
-        </Link>
-    )
+const Button = ({ type, to, text, download }) => {
+
+    if (download ) {
+        return (
+            <a href={to} download={to} className="inline-block">
+                <span className={`button ${type}`}>
+                    <svg width="14" height="17" viewBox="0 0 14 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 17H14V15H0V17ZM14 6H10V0H4V6H0L7 13L14 6Z" className={`${type}-button-icon`} />
+                    </svg>
+
+                    {text}
+                </span>
+            </a>
+        )
+    } else {
+        return (
+            <Link to={to} className={`button uppercase font-bold ${type}`}>
+                {text}
+            </Link>
+        )
+    }
 }
 
 export default Button
