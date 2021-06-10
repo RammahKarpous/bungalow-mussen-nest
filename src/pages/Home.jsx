eb92ba0ec97471352416a8766e59ac3fc9accf3a
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-06-10 09:01:12 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,9 +32,9 @@ const Home = () => {
                 style={{ background: `url(${desktopBgImage})` }}></div>
 
             <div className="absolute bottom-0 left-0 w-full h-96 bg-gradient-to-b from-transparent to-black">
-                <div className="flex flex-col justify-center items-center gap-2 w-72 absolute left-1/2 bottom-10 md:bottom-32 transform -translate-x-1/2 text-center">
+                <div className="flex flex-col justify-center items-center gap-2 w-96 absolute left-1/2 bottom-10 md:bottom-32 transform -translate-x-1/2 text-center">
                     <h1 className="text-white">Welkom</h1>
-                    <p className="text-white">Bij de mussen-nest voor de ideale vakantie voor twee</p>
+                    <p className="text-white font-medium text-largeBodyText">Bij de mussen-nest voor de ideale vakantie voor twee</p>
                     <Button to="/gallerij/tuin" type="secondary" text="Bezoek de gallerij" />
                 </div>
             </div>
