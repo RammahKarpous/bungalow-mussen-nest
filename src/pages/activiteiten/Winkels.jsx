816b1931001188e90e79b01ba0b8b5a7d63a5607
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-06-15 15:56:31 +0100

    Removed content template

--- src/pages/activiteiten/Winkels.jsx
+++ src/pages/activiteiten/Winkels.jsx
@@ -1,6 +1,5 @@
 import MainTemplate from "../../components/templates/Galerij/MainTemplate"
 import ActiviteitenMap from '../../components/activiteiten/ActiviteitenMap';
-import ContentTemplate from './../../components/templates/Galerij/ContentTemplate';
 
 const Winkels = () => {
     return (
