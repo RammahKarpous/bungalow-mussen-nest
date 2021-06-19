8c4f209ba490caa3c5a4159d91ee9080322c4875
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-06-19 11:11:34 +0100

    Update Activiteiten.jsx

--- src/pages/Activiteiten.jsx
+++ src/pages/Activiteiten.jsx
@@ -5,8 +5,10 @@ import { activiteitenLinks } from './../components/links';
 const Activiteiten = ({ children, active }) => {
     return (
         <PageTemplate extraClassNames="col-start-1 col-end-9 md:col-start-2 md:col-end-8 mb-0">
-            <h1>Activiteiten</h1>
-            <TopNavigation type={activiteitenLinks} active={active} />
+            <div className="w-10/12 m-auto md:w-full">
+                <h1>Activiteiten</h1>
+                <TopNavigation type={activiteitenLinks} active={active} />
+            </div>
             {children}
         </PageTemplate>
     )
