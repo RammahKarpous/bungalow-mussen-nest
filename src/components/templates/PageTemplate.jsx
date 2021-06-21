1bbd7f0a151cd720bfad45be6108f8c317017112
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-06-21 16:05:50 +0100

    Partially implemented the page transition for the other pages

--- src/components/templates/PageTemplate.jsx
+++ src/components/templates/PageTemplate.jsx
@@ -1,8 +1,23 @@
+import { motion } from 'framer-motion'
+
 const PageTemplate = ({children, extraClassNames}) => {
+
+    const transition = {
+        duration: .5, 
+        sease: [0.6, .01, -0.05, 0.9 ]
+    }
+
     return (
-        <div className={`page-template ${extraClassNames}`}>
+        <motion.div 
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
+            
+            transition={transition}
+            
+            className={`page-template ${extraClassNames}`}>
             {children}
-        </div>
+        </motion.div>
     )
 }
 
