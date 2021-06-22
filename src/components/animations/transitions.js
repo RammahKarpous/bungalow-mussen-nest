96713a0c3ec5b62173d88e77d0a455db2733c521
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-06-22 13:51:23 +0100

    Created transitions and variants

---
+++ src/components/animations/transitions.js
@@ -0,0 +1,4 @@
+export const defaultTransition = {
+	duration: 0.5,
+	sease: [ 0.6, 0.01, -0.05, 0.9 ]
+};
---
+++ src/components/animations/variants.js
@@ -0,0 +1,12 @@
+export const fade = {
+	initial: {
+		opacity: 0
+	},
+	animate: {
+		opacity: 1
+	},
+	exit: {
+		opacity: 0
+	}
+};
+export const fadeAndSlide = {};
