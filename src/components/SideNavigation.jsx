bc297a92164cd3f2027fd75563f1651442fe3ba4
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-06-23 15:42:19 +0100

    Installed GSAP and animated the navigation with it

--- src/components/SideNavigation.jsx
+++ src/components/SideNavigation.jsx
@@ -1,23 +1,47 @@
-import logo from '../assets/logos/mussen-nest-logo-dark.svg'
 import { NavLink } from "react-router-dom"
 import { links } from './links'
+import { useEffect, useRef, useState } from "react"
+import { gsap } from 'gsap'
 
-// Social media icons
+// Images
+import logo from '../assets/logos/mussen-nest-logo-dark.svg'
 import tripadvisor from '../assets/icons/social-media/tripadvisor-label.svg'
-import { useState } from "react"
 
-const SideNavigation = ({ openState }) => {
+const SideNavigation = () => {
 
-    const [isOpen, setIsOpen] = useState(openState)
+    const [width, setWidth] = useState(window.innerWidth);
+
+    const updateDimensions = () => {
+        setWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", updateDimensions);
+        return () => window.removeEventListener("resize", updateDimensions);
+
+        //eslint-disable-next-line
+    }, []);
+
+    let navigation = useRef(null);
+
+    const hide = () => {
+        gsap.to(navigation, {duration: .3, left: -288})
+    }
+
+    const show = () => {
+        gsap.to(navigation, {duration: .3, left: 0})
+    }
 
     return (
         <div>
-            <div className="h-20 w-20 bg-green-100 fixed text-center text-white z-10 cursor-pointer" onClick={() => setIsOpen(true)}>
+            <div className="h-20 w-20 bg-green-100 fixed text-center text-white z-10 cursor-pointer" onClick={() => show()}>
                 <h4 className="font-semibold text-white absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-largeBodyText uppercase tracking-widest">menu</h4>
             </div>
 
-            <nav className={`fixed w-72 h-full ${isOpen ? 'left-0' : '-left-72'} lg:left-0 transition duration-1000 top-0 bg-green-100 z-20`}>
-                <div className="absolute top-4 left-6 h-10 w-10 lg:invisible cursor-pointer" onClick={() => setIsOpen(false)}>
+            <nav 
+                ref={el => navigation = el}
+                className={`fixed w-72 h-full transition duration-1000 top-0 bg-green-100 z-20`}>
+                <div className="absolute top-4 left-6 h-10 w-10 lg:invisible cursor-pointer" onClick={() => hide()}>
                     <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 rotate-45 bg-white w-1 h-9"></div>
                     <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 -rotate-45 bg-white w-1 h-9"></div>
                 </div>
@@ -29,8 +53,8 @@ const SideNavigation = ({ openState }) => {
                         <NavLink 
                             exact={true}
                             className="flex items-center font-body justify-start px-6 py-5 gap-2 text-white"
-                            activeClassName="bg-green-200" 
-                            onClick={() => setIsOpen(false)} key={i} to={ link.to }>
+                            activeClassName="bg-green-200"
+                            onClick={() => width < 1024 && hide()} key={i} to={ link.to }>
                             <img src={link.icon} alt={`${link.name} icon`} />
                             { link.name }
                         </NavLink>
