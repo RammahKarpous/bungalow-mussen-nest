be154210d39ceb52623299ad475b4fa8a4f2d3b7
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-06-26 15:06:20 +0100

    Replaced the download button svg code with an image

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import icon from '../assets/icons/download.svg'
 
 const Button = ({ type, to, text, download }) => {
 
@@ -6,9 +7,7 @@ const Button = ({ type, to, text, download }) => {
         return (
             <a href={to} download={to} className="inline-block">
                 <span className={`button ${type}`}>
-                    <svg width="14" height="17" viewBox="0 0 14 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0 17H14V15H0V17ZM14 6H10V0H4V6H0L7 13L14 6Z" className={`${type}-button-icon`} />
-                    </svg>
+                    <img src={icon} alt="Download" />
 
                     {text}
                 </span>
